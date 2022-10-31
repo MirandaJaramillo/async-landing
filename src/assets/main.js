2c68617ce474fa3da9bfd41541a7afe44fab0b84
Miranda Jaramillo Morales <mirandajaramillo@MacBook-Pro-de-Miranda-2.local>
2022-10-31 14:36:10 -0600

    Async function to embbed fetched videos into HTML file into Landing Page

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,5 +18,35 @@ async function fetchData(urlApi) {
   const data = await response.json();
   return data;
 }
-
 //when it loads, it executes our function
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items
+      .map(
+        (video) =>
+          `
+        <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+            title
+          </h3>
+        </div>
+      </div>     
+        `
+      )
+      .slice(0, 4)
+      .join('')} 
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
